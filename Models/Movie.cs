53861dc2e49ce40b59676804530747ab5fb738fa
Jake Gambino <jake.gambino1@gmail.com>
2019-10-08 14:27:16 -0500

    init round 2

---
+++ Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieProjectRemade.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string DirectorName { get; set; }
+    }
+}
