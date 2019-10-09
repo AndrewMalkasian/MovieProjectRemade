8f34f975c59c752451b15b7c9e2756687293a809
Misty Malkasian <andrewandmisty@gmail.com>
2019-10-09 10:31:27 -0500

    completed moviecontroller post method

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using MovieProjectRemade.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,11 +34,27 @@ namespace MovieProjectRemade.Controllers
         }
 
         // POST: api/Movies
-        public void Post([FromBody]string value)
+        //public void Post([FromBody]string value)
+        //{
+        //    var movie = new Movie();
+        //    movie.Title = value;
+        //    movie.Genre = value;
+        //    movie.DirectorName = value;
+        //}
+        [HttpPost]
+        public object Post([FromBody]string jsonString)
         {
+            // add reference to Newtonsoft.Json
+            //  using Newtonsoft.Json;
 
+            // jsonString to myJsonObj
+            var myJsonObj = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(jsonString);
+
+            // value1 is myJsonObj[key1]
+            var valueOfkey1 = myJsonObj["key1"];
+
+            return myJsonObj;
         }
-
         // PUT: api/Movies/5
         public void Put(int id, [FromBody]string value)
         {
@@ -47,6 +64,9 @@ namespace MovieProjectRemade.Controllers
         // DELETE: api/Movies/5
         public void Delete(int id)
         {
+            var movie = context.Movies.Where(m => m.Id == id).SingleOrDefault();
+            context.Movies.Remove(movie);
+            context.SaveChanges();
 
         }
     }
