61993a058a86e3c9e0103ecafbeccbd8592b12d4
Jake Gambino <jake.gambino1@gmail.com>
2019-10-10 11:58:47 -0500

    delete method and button functioning

--- FrontEnd/scripts.js
+++ FrontEnd/scripts.js
@@ -13,7 +13,18 @@ function submitForm(){
     contentType: 'application/json',
     data: JSON.stringify(newMovie),
     success: function( data, textStatus, jQxhr ){
-      $('#movie-table').prepend( "<tr><td>" + newMovie.Id + "</td><td>" + newMovie.Title +"</td><td>" + newMovie.Genre + "</td><td>" + newMovie.DirectorName + "</td><td><a href='#' onclick='getMovieList()'><button class='btn-small'>Update Entry</button></a></td></tr>" );
+      $('#movie-table').prepend(
+        "<tr>" +
+          "<td>" + newMovie.Title +"</td>" + 
+          "<td>" + newMovie.Genre + "</td>" + 
+          "<td>" + newMovie.DirectorName + "</td>" +
+          "<td>" + 
+            "<a href='#' onclick='getMovieList()'>" + 
+              "<button class='btn-small'>Update Entry</button>" + 
+            "</a>" + 
+            "<button class='btn-small' onclick='deleteMovie(" + returnedList[i].Id + ")'>Delete Entry</button>" +
+          "</td>" + 
+        "</tr>");
       alert('movie successfully added');
     },
     error: function( jqXhr, textStatus, errorThrown ){
@@ -35,12 +46,14 @@ function getMovieList(){
           returnedList[i] = data[i];
           $('#movie-table').append(
           	"<tr id = 'movie-id-" + returnedList[i].Id + "'>" +
-          		"<td>"      + returnedList[i].Title +
-          		"</td><td>" + returnedList[i].Genre + 
-          		"</td><td>" + returnedList[i].DirectorName + 
-          		"</td><td><a href='#' onclick='getMovieList()'>" +
-          		"<button class='btn-small'>Update Entry</button></a>" +
-          		"</td></tr>");
+              "<td>" + returnedList[i].Title + "</td>" +
+              "<td>" + returnedList[i].Genre + "</td>" + 
+              "<td>" + returnedList[i].DirectorName + "</td>" + 
+              "<td>" +
+                "<button class='btn-small'>Update Entry</button>" +
+                "<button class='btn-small' onclick='deleteMovie(" + returnedList[i].Id + ")'>Delete Entry</button>" +
+              "</td>" +
+            "</tr>");
         }
       })
     },
@@ -48,6 +61,20 @@ function getMovieList(){
       console.log(errorThrown);
     }
   })
+}
+
+function deleteMovie(id){
+  $.ajax({
+    url: 'https://localhost:44384/api/movies/' + id,
+    type: 'delete',
+    contentType: 'application/json',
+    success: function( data, textStatus, jQxhr ){
+      console.log('DELETED THAT SHIT');
+    },
+    error: function( jqXhr, textStatus, errorThrown ){
+      console.log( errorThrown );
+    }
+  });
 }
 
 // function updateMovie(){
@@ -66,30 +93,26 @@ function getMovieList(){
   // });
 // }
 
-function updateMovie(){
+// function updateMovie(){
 
-let newMovie = {
+// let newMovie = {
+// 	Title: document.getElementById('movie-title').value,
+// 	Genre: document.getElementById('movie-genre').value,
+// 	DirectorName: document.getElementById('movie-director').value
+// }
 
-	Title: document.getElementById('movie-title').value,
-	Genre: document.getElementById('movie-genre').value,
-	DirectorName: document.getElementById('movie-director').value
-}
-
-$.ajax({
-  url: 'https://localhost:44384/api/movies'
-  type: 'get'//?
-  dataType: 'json'
-  contentType: 'application/json'
-  success: function(data, textStatus, jqxhr){
-  	$('#').on('click', function(){
-  		"</td><td>" + newMovie.Title +
-  		"</td><td>" + newMovie.Genre + 
-  		"</td><td>" + newMovie.DirectorName +
-  		}
-  	}
-  }
-
-
-})
-
-}
+// $.ajax({
+//   url: 'https://localhost:44384/api/movies',
+//   type: 'get',
+//   dataType: 'json',
+//   contentType: 'application/json',
+//   success: function(data, textStatus, jqxhr){
+//   	$('#').on('click', function(){
+//   		"</td><td>" + newMovie.Title +
+//   		"</td><td>" + newMovie.Genre + 
+//   		"</td><td>" + newMovie.DirectorName +
+//   		}
+//   	}
+//   }
+// })
+// }
