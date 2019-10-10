653dd2fb88e7cb7cc947c001c2c7aaafd5819068
Misty Malkasian <andrewandmisty@gmail.com>
2019-10-10 09:13:17 -0500

    getMovieList scripts.js function updated

--- FrontEnd/scripts.js
+++ FrontEnd/scripts.js
@@ -33,7 +33,14 @@ function getMovieList(){
       $('#movie-table').each(function(){
         for(i = 0; i < data.length; i++){
           returnedList[i] = data[i];
-          $('#movie-table').append("<tr><td>" + returnedList[i].Id + "</td><td>" + returnedList[i].Title +"</td><td>" + returnedList[i].Genre + "</td><td>" + returnedList[i].DirectorName + "</td><td><a href='#' onclick='getMovieList()'><button class='btn-small'>Update Entry</button></a></td></tr>");
+          $('#movie-table').append(
+          	"<tr id = 'movie-id-" + returnedList[i].Id + "'>" +
+          		"<td>"      + returnedList[i].Title +
+          		"</td><td>" + returnedList[i].Genre + 
+          		"</td><td>" + returnedList[i].DirectorName + 
+          		"</td><td><a href='#' onclick='getMovieList()'>" +
+          		"<button class='btn-small'>Update Entry</button></a>" +
+          		"</td></tr>");
         }
       })
     },
@@ -43,20 +50,46 @@ function getMovieList(){
   })
 }
 
-function updateMovie(){
-  $.ajax({
-    url: 'https://localhost:44384/api/movies/',
-    type: 'post',
-    dataType: 'json',
-    contentType: 'application/json',
-    data: JSON.stringify(newMovie),
-    success: function( data, textStatus, jQxhr ){
-      $('#response pre').html( JSON.stringify(newMovie) );
-    },
-    error: function( jqXhr, textStatus, errorThrown ){
-      console.log( errorThrown );
-    }
-  });
+// function updateMovie(){
+  // $.ajax({
+  //   url: 'https://localhost:44384/api/movies/',
+  //   type: 'post',
+  //   dataType: 'json',
+  //   contentType: 'application/json',
+  //   data: JSON.stringify(newMovie),
+  //   success: function( data, textStatus, jQxhr ){
+  //     $('#response pre').html( JSON.stringify(newMovie) );
+  //   },
+  //   error: function( jqXhr, textStatus, errorThrown ){
+  //     console.log( errorThrown );
+  //   }
+  // });
+// }
 
+function updateMovie(){
+
+let newMovie = {
+
+	Title: document.getElementById('movie-title').value,
+	Genre: document.getElementById('movie-genre').value,
+	DirectorName: document.getElementById('movie-director').value
+}
+
+$.ajax({
+  url: 'https://localhost:44384/api/movies'
+  type: 'get'//?
+  dataType: 'json'
+  contentType: 'application/json'
+  success: function(data, textStatus, jqxhr){
+  	$('#').on('click', function(){
+  		"</td><td>" + newMovie.Title +
+  		"</td><td>" + newMovie.Genre + 
+  		"</td><td>" + newMovie.DirectorName +
+  		}
+  	}
+  }
+
+
+})
 
 }
